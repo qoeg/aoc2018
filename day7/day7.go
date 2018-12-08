67f3b47ac13db709cf08b3ed8238e21b849eb10a
Geoff Sullivan <geoffssullivan@gmail.com>
2018-12-07 21:42:06 -0500

    Cleans day7 code

--- day7/day7.go
+++ day7/day7.go
@@ -10,22 +10,29 @@ type step struct {
 	duration int
 }
 
-func firstAlpha(steps []*step) (int, *step) {
-	var idx int
+func remove(slice *[]*step, item *step) *step {
+	s := *slice
+	for i, v := range s {
+		if v == item {
+			*slice = append(s[:i], s[i+1:]...)
+		}
+	}
+	return item
+}
+
+func firstAlpha(steps []*step) *step {
 	var step *step
-	for i, s := range steps {
+	for _, s := range steps {
 		if step == nil {
 			step = s
-			idx = i
 			continue
 		}
 
 		if rune(s.char[0]) < rune(step.char[0]) {
 			step = s
-			idx = i
 		}
 	}
-	return idx, step
+	return step
 }
 
 func parseSteps(input []string, baseDuration int) map[string]*step {
@@ -46,12 +53,9 @@ func parseSteps(input []string, baseDuration int) map[string]*step {
 		if (steps[this] == nil) {
 			steps[this] = &step{
 				char: this,
-				pre: []*step{
-					steps[pre],
-				},
+				pre: []*step{},
 				duration: int(this[0])-64+baseDuration,
 			}
-			continue
 		}
 		
 		steps[this].pre = append(steps[this].pre, steps[pre])
@@ -60,59 +64,51 @@ func parseSteps(input []string, baseDuration int) map[string]*step {
 	return steps
 }
 
+func updateReadyState(todos *map[string]*step, ready *[]*step, done []*step) {
+	for _, step := range *todos {
+		if len(done) > 0 {
+			remove(&step.pre, done[len(done)-1])
+		}
+
+		if len(step.pre) == 0 {
+			*ready = append(*ready, step)
+			delete(*todos, step.char)
+		}
+	}
+}
+
+func updateWorkerState(ready, workers, done *[]*step) {
+	w := *workers
+	for i := range w {
+		if w[i] == nil {
+			if len(*ready) == 0 {
+				continue
+			}
+			w[i] = remove(ready, firstAlpha(*ready))
+		} 
+
+		w[i].duration--
+		if w[i].duration == 0 {
+			*done = append(*done, w[i])
+			w[i] = nil
+		}
+	}
+}
+
 // OrderSteps processes the input steps in the order required by the text.
 // The function also allows adding more workers and setting a base duration for each step.
 func OrderSteps(input []string, numWorkers, baseDuration int) (string, int) {
-	steps := parseSteps(input, baseDuration)
+	todos := parseSteps(input, baseDuration)
+	total := len(todos)
 	
 	done := []*step{}
 	ready := []*step{}
-	count := len(steps)
-	seconds := 0
 	workers := make([]*step, numWorkers)
+	seconds := 0
 
-	for len(done) < count {
-		// find ready steps
-		for _, step := range steps {
-			if len(done) > 0 {
-				for i, item := range step.pre {
-					if item == done[len(done)-1] {
-						step.pre = append(step.pre[:i], step.pre[i+1:]...)
-					}
-				}
-			}
-
-			if len(step.pre) == 0 {
-				ready = append(ready, step)
-				delete(steps, step.char)
-			}
-		}
-
-		// assign work
-		for idx, worker := range workers {
-			if len(ready) > 0 && worker == nil {
-				i, step := firstAlpha(ready)
-
-				workers[idx] = step
-				ready = append(ready[:i], ready[i+1:]...)
-			}
-		}
-
-		// do work
-		for idx, work := range workers {
-			if work == nil {
-				continue
-			}
-
-			work.duration--
-
-			if work.duration == 0 {
-				done = append(done, work)
-				workers[idx] = nil
-			}
-		}
-
-		// tick
+	for len(done) < total {
+		updateReadyState(&todos, &ready, done)
+		updateWorkerState(&ready, &workers, &done)
 		seconds++
 	}
 
